43e80a51d749c444c799f0cdf4f17c4933a7478f
hzrbasaran <hzrbasaran@hotmail.com>
2023-08-13 14:53:00 +0300

    AutoFocus for filter input

--- components/SelectList.tsx
+++ components/SelectList.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import {
     View,
     Text,
@@ -8,244 +8,236 @@ import {
     ScrollView,
     Animated,
     TextInput,
+    TouchableWithoutFeedback,
 } from 'react-native';
 
 import { SelectListProps } from '..';
 
 type L1Keys = { key?: any; value?: any; disabled?: boolean | undefined }
 
-const SelectList: React.FC<SelectListProps> =  ({
-        setSelected,
-        placeholder,
-        boxStyles,
-        inputStyles,
-        dropdownStyles,
-        dropdownItemStyles,
-        dropdownTextStyles,
-        maxHeight,
-        data,
-        defaultOption,
-        searchicon = false,
-        arrowicon = false,
-        closeicon = false,
-        search = true,
-        searchPlaceholder = "search",
-        notFoundText = "No data found",
-        disabledItemStyles,
-        disabledTextStyles,
-        onSelect = () => {},
-        save = 'key',
-        dropdownShown = false,
-        fontFamily
-    }) => {
-
+const SelectList: React.FC<SelectListProps> = ({
+    setSelected,
+    placeholder,
+    boxStyles,
+    inputStyles,
+    dropdownStyles,
+    dropdownItemStyles,
+    dropdownTextStyles,
+    maxHeight,
+    data,
+    defaultOption,
+    searchicon = false,
+    arrowicon = false,
+    closeicon = false,
+    search = true,
+    searchPlaceholder = "search",
+    notFoundText = "No data found",
+    disabledItemStyles,
+    disabledTextStyles,
+    onSelect = () => { },
+    save = 'key',
+    dropdownShown = false,
+    fontFamily
+}) => {
     const oldOption = React.useRef(null)
-    const [_firstRender,_setFirstRender] = React.useState<boolean>(true);
+    const [_firstRender, _setFirstRender] = React.useState<boolean>(true);
     const [dropdown, setDropdown] = React.useState<boolean>(dropdownShown);
     const [selectedval, setSelectedVal] = React.useState<any>("");
-    const [height,setHeight] = React.useState<number>(200)
+    const [height, setHeight] = React.useState<number>(200)
     const animatedvalue = React.useRef(new Animated.Value(0)).current;
-    const [filtereddata,setFilteredData] = React.useState(data)
+    const [filtereddata, setFilteredData] = React.useState(data)
 
 
     const slidedown = () => {
         setDropdown(true)
-        Animated.timing(animatedvalue,{
-            toValue:height,
-            duration:500,
-            useNativeDriver:false,
-            
+        Animated.timing(animatedvalue, {
+            toValue: height,
+            duration: 500,
+            useNativeDriver: false,
         }).start()
     }
     const slideup = () => {
-        
-        Animated.timing(animatedvalue,{
-            toValue:0,
-            duration:500,
-            useNativeDriver:false,
-            
+        Animated.timing(animatedvalue, {
+            toValue: 0,
+            duration: 500,
+            useNativeDriver: false,
+
         }).start(() => setDropdown(false))
     }
 
-    React.useEffect( () => {
-        if(maxHeight)
+    React.useEffect(() => {
+        if (maxHeight)
             setHeight(maxHeight)
-    },[maxHeight])
+    }, [maxHeight])
 
-    
+
     React.useEffect(() => {
         setFilteredData(data);
-      },[data])
+    }, [data])
 
 
     React.useEffect(() => {
-        if(_firstRender){
-          _setFirstRender(false);
-          return;
+        if (_firstRender) {
+            _setFirstRender(false);
+            return;
         }
         onSelect()
-    },[selectedval])
-  
+    }, [selectedval])
+
 
     React.useEffect(() => {
-        if(!_firstRender && defaultOption && oldOption.current != defaultOption.key ){
+        if (!_firstRender && defaultOption && oldOption.current != defaultOption.key) {
             // oldOption.current != null
             oldOption.current = defaultOption.key
             setSelected(defaultOption.key);
             setSelectedVal(defaultOption.value);
         }
-        if(defaultOption && _firstRender && defaultOption.key != undefined){
-            
+        if (defaultOption && _firstRender && defaultOption.key != undefined) {
+
             oldOption.current = defaultOption.key
             setSelected(defaultOption.key);
             setSelectedVal(defaultOption.value);
         }
-        
-    },[defaultOption])
+
+    }, [defaultOption])
 
     React.useEffect(() => {
-        if(!_firstRender){
-            if(dropdownShown)
+        if (!_firstRender) {
+            if (dropdownShown)
                 slidedown();
             else
                 slideup();
-            
         }
-        
-    },[dropdownShown])
+    }, [dropdownShown])
 
 
 
-    return(
-        <View>
-            {
-                (dropdown && search)
-                ?
-                    <View style={[styles.wrapper,boxStyles]}>
-                        <View style={{flexDirection:'row',alignItems:'center',flex:1}}> 
-                            {
-                                (!searchicon)
-                                ?
-                                <Image 
-                                    source={require('../assets/images/search.png')}
-                                    resizeMode='contain'
-                                    style={{width:20,height:20,marginRight:7}}
+    return (
+        <TouchableWithoutFeedback onPress={() => { slideup(); }}>
+            <View style={{flex:1}}>
+                {
+                    (dropdown && search)
+                        ?
+                        <View style={[styles.wrapper, boxStyles]}>
+                            <View style={{ flexDirection: 'row', alignItems: 'center', flex: 1 }}>
+                                {
+                                    (!searchicon)
+                                        ?
+                                        <Image
+                                            source={require('../assets/images/search.png')}
+                                            resizeMode='contain'
+                                            style={{ width: 20, height: 20, marginRight: 7 }}
+                                        />
+                                        :
+                                        searchicon
+                                }
+
+                                <TextInput
+                                    focusable={true}
+                                    autoFocus={true}
+                                    placeholder={searchPlaceholder}
+                                    onChangeText={(val) => {
+                                        let result = data.filter((item: L1Keys) => {
+                                            val.toLowerCase();
+                                            let row = item.value.toLowerCase()
+                                            return row.search(val.toLowerCase()) > -1;
+                                        });
+                                        setFilteredData(result)
+                                    }}
+                                    style={[{ padding: 0, height: 20, flex: 1, fontFamily }, inputStyles]}
                                 />
-                                :
-                                searchicon
-                            }
-                            
-                            <TextInput 
-                                placeholder={searchPlaceholder}
-                                onChangeText={(val) => {
-                                    let result =  data.filter((item: L1Keys) => {
-                                        val.toLowerCase();
-                                        let row = item.value.toLowerCase()
-                                        return row.search(val.toLowerCase()) > -1;
-                                    });
-                                    setFilteredData(result)
-                                }}
-                                style={[{padding:0,height:20,flex:1,fontFamily},inputStyles]}
-                            />
                                 <TouchableOpacity onPress={() => slideup()} >
 
-                                {
-                                    (!closeicon)
-                                    ?
-                                        <Image 
-                                            source={require('../assets/images/close.png')}
-                                            resizeMode='contain'
-                                            style={{width:17,height:17}}
-                                        />
-                                    :
-                                        closeicon
-                                }
-                                   
-                                </TouchableOpacity>
-                                
-                           
-                        </View>
-                        
-                    </View>
-                :
-                    <TouchableOpacity style={[styles.wrapper,boxStyles]} onPress={() => { if(!dropdown){ slidedown() }else{ slideup() } }}>
-                        <Text style={[{fontFamily},inputStyles]}>{ (selectedval == "") ? (placeholder) ? placeholder : 'Select option' : selectedval  }</Text>
-                        {
-                            (!arrowicon)
-                            ?
-                                <Image 
-                                    source={require('../assets/images/chevron.png')}
-                                    resizeMode='contain'
-                                    style={{width:20,height:20}}
-                                />
-                            :
-                                arrowicon
-                        }
-                        
-                    </TouchableOpacity>
-            }
-            
-            {
-                (dropdown)
-                ?
-                    <Animated.View style={[{maxHeight:animatedvalue},styles.dropdown,dropdownStyles]}>
-                        <ScrollView  contentContainerStyle={{paddingVertical:10,overflow:'hidden'}} nestedScrollEnabled={true}>
-
-                            {
-                                (filtereddata.length >=  1)
-                                ?
-                                filtereddata.map((item: L1Keys,index: number) => {
-                                    let key = item.key ?? item.value ?? item;
-                                    let value = item.value ?? item;
-                                    let disabled = item.disabled ?? false;
-                                    if(disabled){
-                                        return(
-                                            <TouchableOpacity style={[styles.disabledoption,disabledItemStyles]} key={index} onPress={ () => {}}>
-                                                <Text style={[{color:'#c4c5c6',fontFamily},disabledTextStyles]}>{value}</Text>
-                                            </TouchableOpacity>
-                                        )
-                                    }else{
-                                        return(
-                                            <TouchableOpacity style={[styles.option,dropdownItemStyles]} key={index} onPress={ () => {
-                                                if(save === 'value'){
-                                                    setSelected(value);
-                                                }else{
-                                                    setSelected(key)
-                                                }
-                                                
-                                                setSelectedVal(value)
-                                                slideup()
-                                                setTimeout(() => {setFilteredData(data)}, 800)
-                                                
-                                            }}>
-                                                <Text style={[{fontFamily},dropdownTextStyles]}>{value}</Text>
-                                            </TouchableOpacity>
-                                        )
+                                    {
+                                        (!closeicon)
+                                            ?
+                                            <Image
+                                                source={require('../assets/images/close.png')}
+                                                resizeMode='contain'
+                                                style={{ width: 17, height: 17 }}
+                                            />
+                                            :
+                                            closeicon
                                     }
-                                    
-                                })
-                                :
-                                <TouchableOpacity style={[styles.option,dropdownItemStyles]} onPress={ () => {
-                                    setSelected(undefined)
-                                    setSelectedVal("")
-                                    slideup()
-                                    setTimeout(() => setFilteredData(data), 800)
-                                    
-                                }}>
-                                    <Text style={[{fontFamily},dropdownTextStyles]}>{notFoundText}</Text>
+
                                 </TouchableOpacity>
+                            </View>
+                        </View>
+                        :
+                        <TouchableOpacity style={[styles.wrapper, boxStyles]} onPress={() => { if (!dropdown) { slidedown() } else { slideup() } }}>
+                            <Text style={[{ fontFamily }, inputStyles]}>{(selectedval == "") ? (placeholder) ? placeholder : 'Select option' : selectedval}</Text>
+                            {
+                                (!arrowicon)
+                                    ?
+                                    <Image
+                                        source={require('../assets/images/chevron.png')}
+                                        resizeMode='contain'
+                                        style={{ width: 20, height: 20 }}
+                                    />
+                                    :
+                                    arrowicon
                             }
-                            
-                            
-                            
-                        </ScrollView>
-                    </Animated.View>
-                :
-                null
-            }
-            
-            
-        </View>
+
+                        </TouchableOpacity>
+                }
+
+                {
+                    (dropdown)
+                        ?
+                        <Animated.View style={[{ maxHeight: animatedvalue }, styles.dropdown, dropdownStyles]}>
+                            <ScrollView contentContainerStyle={{ paddingVertical: 10, overflow: 'hidden' }} nestedScrollEnabled={true}>
+
+                                {
+                                    (filtereddata.length >= 1)
+                                        ?
+                                        filtereddata.map((item: L1Keys, index: number) => {
+                                            let key = item.key ?? item.value ?? item;
+                                            let value = item.value ?? item;
+                                            let disabled = item.disabled ?? false;
+                                            if (disabled) {
+                                                return (
+                                                    <TouchableOpacity style={[styles.disabledoption, disabledItemStyles]} key={index} onPress={() => { }}>
+                                                        <Text style={[{ color: '#c4c5c6', fontFamily }, disabledTextStyles]}>{value}</Text>
+                                                    </TouchableOpacity>
+                                                )
+                                            } else {
+                                                return (
+                                                    <TouchableOpacity style={[styles.option, dropdownItemStyles]} key={index} onPress={() => {
+                                                        if (save === 'value') {
+                                                            setSelected(value);
+                                                        } else {
+                                                            setSelected(key)
+                                                        }
+
+                                                        setSelectedVal(value)
+                                                        slideup()
+                                                        setTimeout(() => { setFilteredData(data) }, 800)
+
+                                                    }}>
+                                                        <Text style={[{ fontFamily }, dropdownTextStyles]}>{value}</Text>
+                                                    </TouchableOpacity>
+                                                )
+                                            }
+
+                                        })
+                                        :
+                                        <TouchableOpacity style={[styles.option, dropdownItemStyles]} onPress={() => {
+                                            setSelected(undefined)
+                                            setSelectedVal("")
+                                            slideup()
+                                            setTimeout(() => setFilteredData(data), 800)
+
+                                        }}>
+                                            <Text style={[{ fontFamily }, dropdownTextStyles]}>{notFoundText}</Text>
+                                        </TouchableOpacity>
+                                }
+                            </ScrollView>
+                        </Animated.View>
+                        :
+                        null
+                }
+            </View>
+        </TouchableWithoutFeedback>
     )
 }
 
@@ -254,9 +246,9 @@ export default SelectList;
 
 
 const styles = StyleSheet.create({
-    wrapper:{ borderWidth:1,borderRadius:10,borderColor:'gray',paddingHorizontal:20,paddingVertical:12,flexDirection:'row',justifyContent:'space-between' },
-    dropdown:{ borderWidth:1,borderRadius:10,borderColor:'gray',marginTop:10,overflow:'hidden'},
-    option:{ paddingHorizontal:20,paddingVertical:8,overflow:'hidden' },
-    disabledoption:{ paddingHorizontal:20,paddingVertical:8,flexDirection:'row',alignItems:'center', backgroundColor:'whitesmoke',opacity:0.9}
+    wrapper: { borderWidth: 1, borderRadius: 10, borderColor: 'gray', paddingHorizontal: 20, paddingVertical: 12, flexDirection: 'row', justifyContent: 'space-between' },
+    dropdown: { borderWidth: 1, borderRadius: 10, borderColor: 'gray', marginTop: 10, overflow: 'hidden' },
+    option: { paddingHorizontal: 20, paddingVertical: 8, overflow: 'hidden' },
+    disabledoption: { paddingHorizontal: 20, paddingVertical: 8, flexDirection: 'row', alignItems: 'center', backgroundColor: 'whitesmoke', opacity: 0.9 }
 
 })
